140b16c32e70b0512799d0273220e1e3219c9839
OfficialGopi <official.gopi@outlook.com>
2025-01-30 14:05:01 +0530

    routes added in user

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
-import { signupUser } from "../controllers/user.controller.js";
+import {
+  loginUser,
+  logoutUser,
+  signupUser,
+} from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const user = Router();
 
 user.route("/sign-up").post(signupUser);
+user.route("/").post(loginUser).delete(verifyJWT, logoutUser);
 
 export { user };
