1c57fb93948bb66a899059e4bc7336e2feb42a67
OfficialGopi <official.gopi@outlook.com>
2025-01-30 18:39:26 +0530

    Post route created

---
+++ src/routes/post.routes.ts
@@ -0,0 +1,5 @@
+import { Router } from "express";
+
+const post = Router();
+
+export { post };
--- src/versions/v1.version.ts
+++ src/versions/v1.version.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { user } from "../routes/user.routes.js";
 import { chat } from "../routes/chat.routes.js";
+import { post } from "../routes/post.routes.js";
 
 const v1 = Router();
 
 v1.use("/user", user);
+v1.use("/post", post);
 v1.use("/chat", chat);
 
 export { v1 };
