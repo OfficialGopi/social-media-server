3c511afad9e723ca1ffc6ab9860eaac59e3011ae
OfficialGopi <official.gopi@outlook.com>
2025-02-03 19:02:04 +0530

    verify added in chat route

--- src/routes/chat.routes.ts
+++ src/routes/chat.routes.ts
@@ -5,11 +5,14 @@ import {
 } from "../controllers/chat.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { sendMessage } from "../controllers/chat-message.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const chat = Router();
 
 chat.route("/personal").post(verifyJWT, createPersonalChat);
 chat.route("/group").post(verifyJWT, createGroupChat);
-chat.route("/send-message").post(verifyJWT, sendMessage);
+chat
+  .route("/send-message")
+  .post(upload.array("attachments", 10), verifyJWT, sendMessage);
 
 export { chat };
